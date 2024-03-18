ecc52ac5c0c86a40ed066f4f2345a06397f529d1
Christopher <chris.dev.software@gmail.com>
2024-03-18 11:25:31 -0500

    Terminando el header solo en version desktop

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#1B3764",
+				secondary: "#FFCA42",
+				degradate: "#F6F8FC"
+			}
+		},
+		fontFamily: {
+			'cardo': ['Cardo', 'serif'],
+			'inter': ['Inter', 'serif']
+		}
+	},
+	plugins: [],
+}
